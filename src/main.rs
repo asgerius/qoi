c9ebd3e3886a65262160a4d5ea408a5494e8b779
Asger Schultz <asger.s@protonmail.com>
2022-12-25 23:06:44 +0100

    Parse cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::process::exit;
+
+fn parse_args() -> String {
+    let args: Vec<String> = std::env::args().collect();
+
+    let input_file = match args.len() {
+        2 => &args[1],
+        _ => {
+            println!("Only one input file can be given");
+            exit(1);
+        },
+    };
+
+    return input_file.to_owned();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input_file = parse_args();
+
+    if input_file.ends_with(".qoi") {
+        println!("Got QOI input file");
+    } else if input_file.ends_with(".png") {
+        println!("Got PNG input file");
+    } else {
+        println!("Unknwon file type");
+        exit(1);
+    }
 }
